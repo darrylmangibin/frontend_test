d559e583d1fb0d6f0adf2d605f67cfe38ad4a630
darryl <darrylmangibin@gmail.com>
2024-02-15 06:42:50 +0800

    Update page and layout to fetch user and pass to gallery

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
+"use client";
+
+import { QueryClientProvider } from "@tanstack/react-query";
 import "./css/index.scss";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { queryClient } from "./utils/queryClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </body>
     </html>
   );
 }
