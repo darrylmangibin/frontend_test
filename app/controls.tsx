054b5b8686ff538e3b0137fe4579d57f9a502e01
darryl <darrylmangibin@gmail.com>
2024-02-15 06:47:50 +0800

    Update control component to change direction and field options

--- app/controls.tsx
+++ app/controls.tsx
@@ -1,15 +1,52 @@
-import Select from "react-select";
+import { FC } from "react";
 
-const Controls = () => {
+import Select, { Props } from "react-select";
+import { User } from "./types/user";
+
+export type FieldOptionKeys = keyof Pick<User, "company" | "name" | "email">;
+export type DirectionOptionKeys = "ascending" | "descending";
+export type FieldOptionType =
+  | {
+      label: string;
+      value: FieldOptionKeys;
+    }
+  | undefined;
+export type DirectionOptionType =
+  | {
+      label: string;
+      value: DirectionOptionKeys;
+    }
+  | undefined;
+
+interface ControlsProps {
+  onChangeField?: Props<
+    { label: string; value: FieldOptionKeys },
+    false
+  >["onChange"];
+  onChangeDirection?: Props<
+    { label: string; value: DirectionOptionKeys },
+    false
+  >["onChange"];
+  fieldValue?: FieldOptionType | null;
+  directionValue?: DirectionOptionType | null;
+}
+
+const Controls: FC<ControlsProps> = ({
+  onChangeField,
+  onChangeDirection,
+  fieldValue,
+  directionValue,
+}) => {
   const fieldOptions = [
     { label: "Name", value: "name" },
     { label: "Company", value: "company" },
     { label: "Email", value: "email" },
-  ];
+  ] satisfies { label: string; value: FieldOptionKeys }[];
+
   const directionOptions = [
     { label: "Ascending", value: "ascending" },
     { label: "Descending", value: "descending" },
-  ];
+  ] satisfies { label: string; value: DirectionOptionKeys }[];
 
   return (
     <div className="gallery-controls controls">
@@ -17,7 +54,13 @@ const Controls = () => {
         <label htmlFor="sort-field" className="label">
           Sort Field
         </label>
-        <Select options={fieldOptions} inputId="sort-field" className="input" />
+        <Select
+          options={fieldOptions}
+          inputId="sort-field"
+          className="input"
+          onChange={onChangeField}
+          value={fieldValue}
+        />
       </div>
       <div className="form-group group">
         <label htmlFor="sort-direction" className="label">
@@ -27,6 +70,8 @@ const Controls = () => {
           options={directionOptions}
           inputId="sort-direction"
           className="input"
+          onChange={onChangeDirection}
+          value={directionValue}
         />
       </div>
     </div>
